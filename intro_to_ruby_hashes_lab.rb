3b71366d934ba5d529fb4646b0c44e3b69dcfb48
Todd Carlson <todd.carlson3030@me.com>
2019-12-18 05:54:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,8 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
   monopoly = {
     key=>
   }
+  
 end
 
 def monopoly_with_second_tier
@@ -11,6 +10,9 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   monopoly = {
+    key=>
+  }
 end
 
 def monopoly_with_third_tier
