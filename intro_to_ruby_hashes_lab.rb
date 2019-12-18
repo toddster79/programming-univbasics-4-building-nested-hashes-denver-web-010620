43fa29a4e699ac84de145720012ed031df778db6
Todd Carlson <todd.carlson3030@me.com>
2019-12-18 06:42:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
-        :key2 => {},
+        :two_pieces_owned => 50,
         :key3 => {},
         :key4 => {}
       }
