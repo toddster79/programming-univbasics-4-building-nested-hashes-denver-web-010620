378ce0702782c3163affee5b1c192a77e3230377
Todd Carlson <todd.carlson3030@me.com>
2019-12-18 06:45:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
     :railroads => {
       :names => {
         :reading_railroad => {},
-        :key2 => {},
+        :pennsylvania_railroad => {},
         :key3 => {},
         :key4 => {}
       },
