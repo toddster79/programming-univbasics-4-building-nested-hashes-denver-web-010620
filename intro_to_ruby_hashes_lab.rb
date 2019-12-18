98e5af8c3bbeea08451fec064ea77e6522670319
Todd Carlson <todd.carlson3030@me.com>
2019-12-18 06:02:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash 
   { 
-    :key => {}
+    :railroads => {}
   }
   
 end
@@ -10,9 +10,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  { 
-    :railroads => {}
-  }
+  
   
 end
 
